6001dc0ff6800e68c06f51e525d48f0341502407
GreedyLucifer <46939658+GreedyLucifer@users.noreply.github.com>
2020-07-02 15:11:02 -0700

    Update CheckVisible.h

--- CheckVisible.h
+++ CheckVisible.h
@@ -1,6 +1,35 @@
 #pragma once
 const  double EPSINON = 0.0000001;
 
+struct point
+{
+	double x, y;
+	int pivot;
+	bool operator < (const point &o) const
+	{
+		return pivot < o.pivot;
+	}
+	bool operator == (const point &o) const
+	{
+		return pivot == o.pivot;
+	}/**/
+};
+
+typedef struct facet
+{
+	point v1, v2;
+	bool operator < (const facet &o) const
+	{
+		if (v1.pivot == o.v1.pivot)
+			return v2.pivot < o.v2.pivot;
+		return v1.pivot < o.v1.pivot;
+	}
+	bool operator == (const facet &o) const
+	{
+		return v1.pivot == o.v1.pivot && v2.pivot == o.v2.pivot;
+	}
+}Facet;
+
 bool visible(point v, facet t) {
 	if (EPSINON < (t.v1.x - v.x) * (t.v2.y - v.y) - (t.v1.y - v.y) * (t.v2.x - v.x)) return true;
 	else return false;
